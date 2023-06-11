27872cb8e2e2bdedf14cf4f00ee6969775358072
Souvik Sarkar <89373683+Souvik2024@users.noreply.github.com>
2023-06-11 19:58:05 +0530

    Add files via upload

---
+++ Count number of subarrays with given xor K.py
@@ -0,0 +1,16 @@
+class Solution: 
+    def solve(self, A, B):
+        h={}
+        count=0
+        xor=0
+        for i in range(len(A)):
+            xor^=A[i]
+            if xor==B:
+                count+=1
+            if xor^B in h:
+                count+=h[xor^B]
+            if xor in h:
+                h[xor]+=1
+            else:
+                h[xor]=1
+        return count 
